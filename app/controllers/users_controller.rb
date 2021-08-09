695196ad33779a5fb40325743a6e447087f94cc3
Mohamed Tawfik <motawfik98@outlook.com>
2021-08-09 13:59:06 +0200

    Implement login endpoint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def login
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      token = encode_token({user_id: user.id})
+      render json: {accessToken: token, message: "success"}
+    else
+      render json: {message: "Invalid credentials", status: 401}, status: :unauthorized
+    end
+  end
+
   private
   def user_params
     params.permit(:fullName, :password, :email, :password_confirmation)
